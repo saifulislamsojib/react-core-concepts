9656a5f60372a07ec396560adaf23e62f5754a98
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-02-23 13:54:26 +0600

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const products =[
+    {name: "Phone", price: "150"},
+    {name: "Laptop", price: "500"},
+    {name: "Watch", price: "100"},
+    {name: "Tab", price: "200"},
+    {name: "mouse", price: "80"}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edited <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Post></Post>
+      <Counter count={count} />
+      {
+        products.map((product, i) => <Product product={product} setCount={setCount} count={count} key={i}></Product>)
+      }
+    </div>
+  );
+}
+
+function Post(){
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res=> res.json())
+    .then(data => setPosts(data))
+  }, [])
+  return (
+    <div>
+      <h2>total Posts: {posts.length}</h2>
+      <ol>
+      {
+        posts.map(post => <li key={post.id}>{post.title}</li>)
+      }
+      </ol>
+    </div>
+  )
+}
+
+function Counter({count}) {
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+    </div>
+  );
+}
+
+function Product({product, setCount, count}) {
+  const {name, price} = product;
+  const productStyle = {
+    color: "red",
+    backgroundColor: "lightgray",
+    padding: "10px",
+    margin: "10px auto",
+    borderRadius: "10px",
+    width: "400px",
+  }
+  const btnStyle = {
+    borderRadius: "7px",
+    border: "none",
+    backgroundColor: "purple",
+    color: "white",
+    padding: "10px 15px",
+    cursor: "pointer"
+  }
+  return(
+    <div style={productStyle}>
+      <h1>Name: {name}</h1>
+      <h2>Price: ${price}</h2>
+      <button onClick={()=> setCount(count + 1)} style={btnStyle}>Buy Now</button>
     </div>
   );
 }
